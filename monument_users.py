9c606efdde6e58ad28503d47c20154ed02af11c6
arjunrawal4 <arjunrawal4@gmail.com>
2018-01-29 14:59:13 -0600

    adding comments

--- monument_users.py
+++ monument_users.py
@@ -1,11 +1,11 @@
 import MySQLdb
 from intercom.client import Client
 
-intercom = Client(personal_access_token='my_personal_access_token') 
+intercom = Client(personal_access_token='my_personal_access_token') #logs into intecom
 
-db = MySQLdb.connect(host="localhost", user="user", passwd="password", db="monument")
+db = MySQLdb.connect(host="localhost", user="user", passwd="password", db="monument") #connects to db
 cur = db.cursor() 
 cur.execute("SELECT * FROM users")
    
 for user in cur.fetchall() :
-    user = intercom.users.create(email=user[1], name=user[2])
+    user = intercom.users.create(email=user[1], name=user[2]) #creates user with email and name
